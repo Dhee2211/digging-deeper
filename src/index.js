62d9795694371910d307337ae6b02d4d8e78f8ff
Dheerendra Panwar <Dhee2211@users.noreply.github.com>
2022-05-16 15:20:49 -0700

    final

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fname = "Dhee";
+const lname = "Panwar";
+const number = Math.floor(Math.random() * 30);
+// old format
+/*ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fname} {lname}
+    </h1>
+    <h2>Your lucky number is {number}</h2>
+  </div>,
+  document.getElementById("root")
+);*/
+
+//es6 fromat
+ReactDOM.render(
+  <div>
+    <h1>Hello {`${fname} ${lname}`}</h1>
+    <h2>Your lucky number is {number}</h2>
+  </div>,
+  document.getElementById("root")
+);
